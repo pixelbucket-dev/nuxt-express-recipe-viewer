9f3077336191e72e05ccb9c46856b2d729c45bb2
Falk Schieber <falk.schieber@oup.com>
2017-09-18 10:06:11 +0100

    Shortens code
    E.g. better ES6 coverage

--- server/api/recipes.js
+++ server/api/recipes.js
@@ -3,30 +3,27 @@ import { recipes } from './recipes.json'
 
 const router = Router()
 
-const listItemSimple = (item) => {
-  const simpleItem = {
-    ...item
-  }
-  simpleItem.ingredients = item.ingredients.reduce((acc, cur) => acc.concat(cur.name), [])
-  return simpleItem
-}
+const listItemSimple = item => ({
+  ...item,
+  ingredients: item.ingredients.reduce((acc, cur) => acc.concat(cur.name), [])
+})
 
 /* GET recipes listing. */
-router.get('/recipes', function (req, res, next) {
+router.get('/recipes', (req, res, next) => {
   if (!req.query.filter || req.query.filter === '') {
     res.json(recipes.map(listItemSimple))
   } else {
     const filterTerm = req.query.filter.toLowerCase()
-    const filteredRecipes = recipes.filter(
-      item => item.name.toLowerCase().indexOf(filterTerm) !== -1
-    ).map(listItemSimple)
+    const filteredRecipes = recipes
+      .filter(item => item.name.toLowerCase().indexOf(filterTerm) !== -1)
+      .map(listItemSimple)
 
     res.json(filteredRecipes)
   }
 })
 
 /* GET recipe by ID. */
-router.get('/recipes/:id', function (req, res, next) {
+router.get('/recipes/:id', (req, res, next) => {
   const id = parseInt(req.params.id)
   if (id >= 0 && id < recipes.length) {
     res.json(recipes[id])
